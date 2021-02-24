ed156fbda6e8f82842c17618e911c3691e83a902
ardinur03 <ardinurinsan03@gmail.com>
2021-02-24 07:56:03 +0700

    component livewire tabel crud siswa

---
+++ resources/views/livewire/hubin/siswa/tbl-siswa.blade.php
@@ -0,0 +1,83 @@
+<div>
+    {{-- Success is as dangerous as failure. --}}
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-outline card-primary mt-3">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ $title }}</h3>
+
+                        <div class="card-tools">
+                            <a href="#" class="btn btn-success btn-sm"><i class="fas fa-user-graduate"></i> <i
+                                    class="fas fa-plus"></i> </a>
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i
+                                    class="fas fa-minus"></i></button>
+                        </div>
+                        <!-- /.card-tools -->
+                    </div>
+                    <!-- /.card-header -->
+
+                    <!-- sweetalert start -->
+                    <div class="swal" data-swal=""></div>
+                    <div class="ggl" data-ggl=""></div>
+                    <!-- sweetalert end -->
+
+                    <div class="card-body">
+                        <div class="container">
+                            <table class="table table-responsive-md table-sm table-hover">
+                                <thead class="bg-primary judul">
+                                    <tr>
+                                        <th scope="col">No</th>
+                                        <th>NIS</th>
+                                        <th>Nama Siswa</th>
+                                        <th>Kelas</th>
+                                        <th>Jenis Kelamin</th>
+                                        <th>Jurusan</th>
+                                        <th>kontak</th>
+                                        <th>Alamat</th>
+                                        <th>Angkatan</th>
+                                        <th scope="colgroup">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php $no = 1; ?>
+                                    @foreach ($siswa as $s)
+                                    <tr>
+                                        <th>{{ $no++ }}</th>
+                                        <td>{{ $s->nis }}</td>
+                                        <td>{{ $s->nama_siswa }}</td>
+                                        <td>{{ $s->kelas }}</td>
+                                        <td>{{ $s->jk_siswa }}</td>
+                                        <td>{{ $s->nama_jurusan }}</td>
+                                        <td>{{ $s->kontak_siswa }}</td>
+                                        <td>{{ $s->alamat }}</td>
+                                        <td>{{ $s->angkatan }}</td>
+                                        <td>
+                                            <div class="btn-group d-inline-flex">
+                                                <a href="" data-toggle="tooltip" data-placement="top" title="Edit"
+                                                    class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
+                                                <button class="btn btn-warning btn-sm text-white" data-tooltip="tooltip"
+                                                    data-toggle="modal"><i class="fas fa-search"></i></button>
+                                                <button data-tooltip="tooltip" data-placement="top" title="Delete"
+                                                    class="btn btn-danger btn-sm"><i
+                                                        class="far fa-trash-alt"></i></button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+            </div>
+            <!-- /.card -->
+        </div>
+
+    </div>
+
+
+</div>
