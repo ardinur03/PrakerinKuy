fdb5744f80c3e38ecb995cdd2987407103a304f3
ardinur03 <ardinurinsan03@gmail.com>
2021-02-24 07:54:07 +0700

    update content-header

--- resources/views/livewire/adminLTE/content-header.blade.php
+++ resources/views/livewire/adminLTE/content-header.blade.php
@@ -1,18 +1,23 @@
 <div>
     <!-- Content Header (Page header) -->
+
+    <!-- Content Header (Page header) -->
     <div class="content-header">
-        <div class="container-fluid">
+        <div class="container">
             <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">{{ Auth::user()->fullname }}</h1>
+                <div class="col-md-6">
+                    {{--  <h1 class="m-0 text-dark">Tabel Siswa</h1>  --}}
                 </div><!-- /.col -->
-                <div class="col-sm-6">
+                <div class="col-md-6">
                     <ol class="breadcrumb float-sm-right">
-
-                        <?php $role = Auth::user()->role ?>
-
-                        <li class="breadcrumb-item"><a href="{{  $role == 1 ? '/admin/dashboard' : ($role == 2) ? '/hubin/dashboard' : ($role == 3 ? '/dashboard' : '') }}"> <i class="fas fa-tachometer-alt"></i> &nbsp; Home</a></li>
-                        <li class="breadcrumb-item active">Starter Page</li>
+                        <li class="breadcrumb-item">
+                            <a href="/hubin" class="text-decoration-none"><i class="fas fa-home"> Dashboard</i>
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item active">
+                            <a class="text-text-decoration-none">
+                                <i class="fas fa-user-graduate"> Tabel Siswa</i> </a>
+                        </li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
