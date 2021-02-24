b9be4d1cf09b67420e73474bd31222397120f1c6
ardinur03 <ardinurinsan03@gmail.com>
2021-02-24 07:54:21 +0700

    create construct

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -3,19 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Siswa;
+use Illuminate\Support\Facades\DB;
 
 class SiswaController extends Controller
 {
     public function __construct()
     {
-        
+        $this->SiswaModel = new Siswa();
     }
 
     public function index()  
     {
         $data = array(
             'title' => 'Dashboarrd | Siswa',
-            'contentHeader' => true
+            'contentHeader' => true,
+            // 'siswa' => $this->SiswaModel->getData(),
         );
         return view('rolesViews/siswa/index_siswa', $data);
     }
