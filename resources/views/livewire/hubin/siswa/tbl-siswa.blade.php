e7ea783ebf54001ebe10729559ccc0e46a062579
ardinur03 <ardinurinsan03@gmail.com>
2021-03-04 21:10:51 +0700

    tambah komponen fillter & search # KAMIS

--- resources/views/livewire/hubin/siswa/tbl-siswa.blade.php
+++ resources/views/livewire/hubin/siswa/tbl-siswa.blade.php
@@ -18,8 +18,24 @@
                     </div>
                     <!-- /.card-header -->
 
+                  
+
                     <div class="card-body">
                         <div class="container">
+                            <div class="row">
+
+                                <div class="col">
+                                    <select wire:model="paginate" id="" class="form-control form-control-sm w-auto">
+                                        <option value="5">5</option>
+                                        <option value="10">10</option>
+                                        <option value="20">20</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-3">
+                                    <input type="text" wire:model="search" class="form-control mb-3 form-control-sm" placeholder="Cari berdasarkan nama siswa ...">
+                                </div>
+
+                            </div>
                             <table class="table table-responsive-md table-sm table-hover">
                                 <thead class="bg-primary judul">
                                     <tr class="text-center">
@@ -36,7 +52,7 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php $no = 1; ?>
+                                    <?php $no = 1 + ($paginate * ($page - 1)); ?>
                                     @foreach ($siswa as $s)
                                     <tr>
                                         <th>{{ $no++ }}</th>
@@ -50,11 +66,11 @@
                                         <td>{{ $s->angkatan }}</td>
                                         <td>
                                             <div class="btn-group d-inline-flex">
-                                                <a href="" data-toggle="tooltip" data-placement="top" title="Edit"
-                                                    class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
+                                                <button wire:click="$emitTo('hsiswa.siswa-update', 'updateSiswa', {{ $s->id }})" data-toggle="tooltip" data-placement="top" title="Edit"
+                                                    class="btn btn-info btn-sm"><i class="fas fa-edit"></i></button>
                                                 <button class="btn btn-warning btn-sm text-white" data-tooltip="tooltip"
                                                     data-toggle="modal"><i class="fas fa-search"></i></button>
-                                                <button data-tooltip="tooltip" data-placement="top" title="Delete"
+                                                <button wire:click="getDelete({{ $s->id }})" data-tooltip="tooltip" data-placement="top" title="Delete"
                                                     class="btn btn-danger btn-sm"><i
                                                         class="far fa-trash-alt"></i></button>
                                             </div>
@@ -64,6 +80,7 @@
 
                                 </tbody>
                             </table>
+                            {{ $siswa->links() }}
                         </div>
                     </div>
                     <!-- /.card-body -->
@@ -73,6 +90,7 @@
         </div>
 
         <livewire:hsiswa.siswa-create :jurusan="$jurusan"></livewire:hsiswa.siswa-create>
+        <livewire:hsiswa.siswa-update :jurusan="$jurusan"></livewire:hsiswa.siswa-update>
     </div>
 
 
@@ -81,6 +99,19 @@
         <script src="{{ asset('vendor/sweetalert/script.alert.js') }}"></script>
     @endpush
 
+    @push('modal-crud')
+        <script>
+            window.addEventListener('closeModal', event => {
+                $("#modal_create_siswa").modal('hide');
+            })
+            window.addEventListener('closeModal', event => {
+                $("#modal_update_siswa").modal('hide');
+            })
+            window.addEventListener('openModal', event => {
+                $("#modal_update_siswa").modal('show');
+            })
+        </script>
+    @endpush
 
     @push('toast-notification-js')
         <script src="{{ asset('vendor\toast\js\toastr.min.js') }}"></script>
