b473374a2af0acf9bbc0eb0a38df5c0400fb71eb
ardinur03 <ardinurinsan03@gmail.com>
2021-03-04 21:09:15 +0700

    tambah paginate & update #KAMIS

--- app/Http/Livewire/ContactIndex.php
+++ app/Http/Livewire/ContactIndex.php
@@ -23,9 +23,9 @@ class ContactIndex extends Component
 
     protected $queryString = ['search'];
 
-    public function mount() 
+    public function mount()
     {
-      $this->search = request()->query('search', $this->search);
+        $this->search = request()->query('search', $this->search);
     }
 
     public function render()
@@ -33,19 +33,21 @@ class ContactIndex extends Component
         // $this->data = Contact::latest()->get();   => cara 1
         return view('livewire.contact-index', [
             'contacts' =>  $this->search === null ?
-                Contact::latest()->paginate($this->paginate) : 
+                Contact::latest()->paginate($this->paginate) :
                 Contact::latest()->where('name', 'like', '%' . $this->search . '%')->paginate($this->paginate)
         ]);
     }
 
-    public function getContact($id) {
+    public function getContact($id)
+    {
         $this->statusUpdate = true;
         $contact = Contact::find($id);
         $this->emit('getContact', $contact);
     }
 
-    public function destroy($id) {
-        if ($id){
+    public function destroy($id)
+    {
+        if ($id) {
             $data = Contact::find($id);
             $data->delete();
             session()->flash('message', 'Contact was deleted !!!');
@@ -54,12 +56,12 @@ class ContactIndex extends Component
 
     public function HandleStore($contact)
     {
-        session()->flash('message', 'Contact '. $contact['name'] .' was stored');
+        session()->flash('message', 'Contact ' . $contact['name'] . ' was stored');
     }
 
     public function HandleUpdate($contact)
     {
         $this->statusUpdate = false;
-        session()->flash('message', 'Contact '. $contact['name'] .' was updated');
+        session()->flash('message', 'Contact ' . $contact['name'] . ' was updated');
     }
 }
