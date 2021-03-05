a9546cbbe58a9f669f5a9cfccf60ed03237e42e2
ardinur03 <ardinurinsan03@gmail.com>
2021-03-05 16:55:47 +0700

    #JUMAT update view tabel dan add js untuk pop up

--- resources/views/livewire/hubin/siswa/tbl-siswa.blade.php
+++ resources/views/livewire/hubin/siswa/tbl-siswa.blade.php
@@ -18,8 +18,6 @@
                     </div>
                     <!-- /.card-header -->
 
-                  
-
                     <div class="card-body">
                         <div class="container">
                             <div class="row">
@@ -32,7 +30,7 @@
                                     </select>
                                 </div>
                                 <div class="col-md-3">
-                                    <input type="text" wire:model="search" class="form-control mb-3 form-control-sm" placeholder="Cari berdasarkan nama siswa ...">
+                                    <input type="text" wire:model="search" class="form-control mb-3 form-control-sm" placeholder="Cari berdasarkan Nama atau NIS siswa ...">
                                 </div>
 
                             </div>
@@ -45,41 +43,48 @@
                                         <th>Kelas</th>
                                         <th>Jenis Kelamin</th>
                                         <th>Jurusan</th>
-                                        <th>kontak</th>
-                                        <th>Alamat</th>
+                                        {{--  <th>kontak</th>  --}}
+                                        {{--  <th>Alamat</th>  --}}
                                         <th>Angkatan</th>
                                         <th scope="colgroup">Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <?php $no = 1 + ($paginate * ($page - 1)); ?>
-                                    @foreach ($siswa as $s)
-                                    <tr>
-                                        <th>{{ $no++ }}</th>
-                                        <td>{{ $s->nis }}</td>
-                                        <td>{{ $s->nama_siswa }}</td>
-                                        <td>{{ $s->kelas }}</td>
-                                        <td>{{ $s->jk_siswa }}</td>
-                                        <td>{{ $s->nama_jurusan }}</td>
-                                        <td>{{ $s->kontak_siswa }}</td>
-                                        <td>{{ $s->alamat }}</td>
-                                        <td>{{ $s->angkatan }}</td>
-                                        <td>
-                                            <div class="btn-group d-inline-flex">
-                                                <button wire:click="$emitTo('hsiswa.siswa-update', 'updateSiswa', {{ $s->id }})" data-toggle="tooltip" data-placement="top" title="Edit"
-                                                    class="btn btn-info btn-sm"><i class="fas fa-edit"></i></button>
-                                                <button class="btn btn-warning btn-sm text-white" data-tooltip="tooltip"
-                                                    data-toggle="modal"><i class="fas fa-search"></i></button>
-                                                <button wire:click="getDelete({{ $s->id }})" data-tooltip="tooltip" data-placement="top" title="Delete"
-                                                    class="btn btn-danger btn-sm"><i
-                                                        class="far fa-trash-alt"></i></button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-
-                                </tbody>
-                            </table>
+                                    @if (count($siswa))
+                                        @foreach ($siswa as $s)
+                                        <tr>
+                                            <th>{{ $no++ }}</th>
+                                            <td>{{ $s->nis }}</td>
+                                            <td>{{ $s->nama_siswa }}</td>
+                                            <td>{{ $s->kelas }}</td>
+                                            <td>{{ $s->jk_siswa }}</td>
+                                            <td>{{ $s->nama_jurusan }}</td>
+                                            {{--  <td>{{ $s->kontak_siswa }}</td>  --}}
+                                            {{--  <td>{{ $s->alamat }}</td>  --}}
+                                            <td>{{ $s->angkatan }}</td>
+                                            <td>
+                                                <div class="btn-group d-inline-flex">
+                                                    <button wire:click="$emitTo('hsiswa.siswa-update', 'updateSiswa', {{ $s->nis }})" data-toggle="tooltip" data-placement="top" title="Edit"
+                                                        class="btn btn-info btn-sm"><i class="fas fa-edit"></i></button>
+                                                    <button wire:click="$emitTo('hsiswa.siswa-detail', 'detailSiswa', {{ $s->nis }})" class="btn btn-warning btn-sm text-white" data-tooltip="tooltip"
+                                                        data-toggle="modal"><i class="fas fa-search"></i></button>
+                                                    <button wire:click="getDelete({{ $s->nis }})" data-tooltip="tooltip" data-placement="top" title="Delete"
+                                                        class="btn btn-danger btn-sm"><i
+                                                            class="far fa-trash-alt"></i></button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                @else 
+                                <div class="row justify-content-center">
+                                    <div class="col">
+                                         <h2>Data tidak di temukan !!!</h2>
+                                    </div>
+                                </div>
+                                @endif
                             {{ $siswa->links() }}
                         </div>
                     </div>
@@ -89,10 +94,21 @@
             <!-- /.card -->
         </div>
 
+        {{--  COMPONENT CREATE UNTUK MODAL/POPUP SISWA  --}}
         <livewire:hsiswa.siswa-create :jurusan="$jurusan"></livewire:hsiswa.siswa-create>
+
+        {{--  COMPONENT UPDATE UNTUK MODAL/POPUP SISWA  --}}
         <livewire:hsiswa.siswa-update :jurusan="$jurusan"></livewire:hsiswa.siswa-update>
+
+        {{--  COMPONENT DETAIL UNTUK MODAL/POPUP SISWA  --}}
+        <livewire:hsiswa.siswa-detail :jurusan="$jurusan"></livewire:hsiswa.siswa-detail>
+
     </div>
 
+
+    @push('css-animate')
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    @endpush
 
     @push('sweet-alert-js')
         <script src="{{ asset('vendor/sweetalert/sweetalert2.all.min.js') }}"></script>
@@ -104,11 +120,14 @@
             window.addEventListener('closeModal', event => {
                 $("#modal_create_siswa").modal('hide');
             })
-            window.addEventListener('closeModal', event => {
+            window.addEventListener('closeModalUpdate', event => {
                 $("#modal_update_siswa").modal('hide');
             })
             window.addEventListener('openModal', event => {
                 $("#modal_update_siswa").modal('show');
+            })
+            window.addEventListener('openModalDetail', event => {
+                $("#modal_detail_siswa").modal('show');
             })
         </script>
     @endpush
