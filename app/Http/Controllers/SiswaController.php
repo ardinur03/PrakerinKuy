c7a49cb974c7c70e9f39969e5fb794d31af35cec
ardinur03 <ardinurinsan03@gmail.com>
2021-03-12 14:43:19 +0700

    update

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Siswa;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class SiswaController extends Controller
@@ -11,15 +13,25 @@ class SiswaController extends Controller
     public function __construct()
     {
         $this->SiswaModel = new Siswa();
+        $this->UserModel = new User();
     }
 
-    public function index()  
+    public function index()
     {
         $data = array(
-            'title' => 'Dashboarrd | Siswa',
+            'title' => 'Dashboard | Siswa',
             'contentHeader' => true,
+            'dataUser' => $this->UserModel->getDataUser(Auth::user()->id)
             // 'siswa' => $this->SiswaModel->getData(),
         );
+
+        dd($data['dataUser']);
+
         return view('rolesViews/siswa/index_siswa', $data);
+    }
+
+    public function coba()
+    {
+        return view('home');
     }
 }
