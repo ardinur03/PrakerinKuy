115e92f45e38a66eeef4baa18ba367886a60caa3
ardinur03 <ardinurinsan03@gmail.com>
2021-03-12 14:48:30 +0700

    membuat function data tiap role user #JUMAT

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -40,4 +41,19 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getDataUser($id)
+    {
+        return DB::table('users')->join('siswa', 'users.id', '=', 'siswa.user_id')
+            ->join('jurusan', 'siswa.jurusan_id', '=', 'jurusan.id')
+            ->where('users.id', $id)
+            ->get();
+    }
+
+    public function getDataAdminHubin($id)
+    {
+        return DB::table('users')->leftJoin('admin_hubin', 'users.id', '=', 'admin_hubin.user_id')
+            ->where('users.id', $id)
+            ->get();
+    }
 }
