e6929bd67a6b0b235811fb5ba5fb7ad6ee13f072
ardinur03 <ardinurinsan03@gmail.com>
2021-03-19 13:23:19 +0700

    panggil plugin excel #jumat

--- app/Http/Livewire/Hsiswa/TblSiswa.php
+++ app/Http/Livewire/Hsiswa/TblSiswa.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Livewire\Hsiswa;
 
+use App\Exports\SiswaExportExcel;
 use App\Models\{Siswa, Jurusan};
 use Illuminate\Support\Facades\Storage;
 use Livewire\{Component, WithPagination};
+use Maatwebsite\Excel\Facades\Excel;
 
 class TblSiswa extends Component
 {
@@ -24,7 +26,9 @@ class TblSiswa extends Component
         'reloadTblSiswa' => '$refresh',
         'siswaStoreSuccess'  => 'handleSiswaCreateSuccess',
         'siswaUpdateSuccess' => 'handleSiswaUpdateSuccess',
+        'siswaCreateAccountSuccess' => 'handleSiswaCreateAccountSuccess',
         'siswaStoreFail'     => 'handleSiswaCreateFail',
+        'siswaStoreAccountFail'     => 'handleSiswaCreateAccountFail',
         'DeleteSiswa'   => 'destroy',
         'destroyLIstSiswaJs'  => 'destroyLIstSiswa'
     ];
@@ -42,8 +46,10 @@ class TblSiswa extends Component
                 ->select('siswa.*', 'jurusan.nama_jurusan')->paginate($this->paginate) : Siswa::latest()->leftjoin('jurusan', 'siswa.jurusan_id', '=', 'jurusan.id')
                 ->select('siswa.*', 'jurusan.nama_jurusan')->where('nama_siswa', 'like', '%' . $this->search . '%')->orWhere('nis', 'like', '%' . $this->search . '%')->paginate($this->paginate),
             'title' => 'Master data | Siswa',
+            'siswaExcel' => $this->SiswaModel->getDataSiswaExcel(),
             'jurusan' => $this->JurusanModel->getDataJurusan(),
         );
+
         // $this->emit('destroy');
         return view('livewire.hubin.siswa.tbl-siswa', $data)
             ->extends('layouts.After_Login.app_backend', $data)
@@ -120,6 +126,19 @@ class TblSiswa extends Component
         ]);
     }
 
+    public function handleSiswaCreateAccountSuccess($nama_siswa)
+    {
+        //panggil sweetalert sukses
+        $this->emit('alert-success', [
+            'position' => 'center',
+            'type'  => 'success',
+            'icon'  => 'success',
+            'title' => 'Berhasil di buatkan akun !!!',
+            'showConfirmButton' => true,
+            'text'  => 'Siswa ' . '<b>' . $nama_siswa . '</b>' . ' Email username & password  berhasil di kirim ke siswa !!!',
+        ]);
+    }
+
     public function handleSiswaUpdateSuccess($siswa)
     {
         //panggil sweetalert sukses
@@ -146,5 +165,24 @@ class TblSiswa extends Component
             'showConfirmButton' => true,
             'text'  => '<b>' . $data . '</b>',
         ]);
+    }
+
+    public function handleSiswaCreateAccountFail($data)
+    {
+        //panggil sweetalert sukses
+        $this->emit('alert-success', [
+            'position' => 'center',
+            'type'  => 'error',
+            'icon'  => 'error',
+            'title' => 'error !!!',
+            'timer' =>  false,
+            'showConfirmButton' => true,
+            'text'  => 'Siswa dengan nama : ' . '<b>' .  $data .  '</b>' . ' sudah terdaftar di akun S11Prakerin Kuy !!!',
+        ]);
+    }
+
+    public function exportSiswaExcel()
+    {
+        return Excel::download(new SiswaExportExcel, 'Siswa.xlsx');;
     }
 }
