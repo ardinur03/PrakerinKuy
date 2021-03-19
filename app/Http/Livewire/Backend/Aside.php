117c7c8e4c4e3fd50490860d4443ec0171f9e1f9
ardinur03 <ardinurinsan03@gmail.com>
2021-03-19 13:18:25 +0700

    create method mount #jumat

--- app/Http/Livewire/Backend/Aside.php
+++ app/Http/Livewire/Backend/Aside.php
@@ -6,6 +6,14 @@ use Livewire\Component;
 
 class Aside extends Component
 {
+
+    public $nama_siswa;
+
+    public function mount($dataUser)
+    {
+        $this->nama_siswa = $dataUser[0]->nama_siswa;
+    }
+
     public function render()
     {
         return view('livewire.adminLTE.aside');
